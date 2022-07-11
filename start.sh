ec23bb22bf81073efd9e4153c6c430a54536eb61
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2022-07-11 17:47:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/LISA-KOREA/MUSIC-CHAT-BOT-V2 /RadioPlayerV3
+git clone https://github.com/LISA-KOREA/MUSIC-CHAT-BOT-V2 /MusicChatBotV2
 echo "Installing Requirements..."
-cd /RadioPlayerV3
+cd /MusicChatBotV2
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
